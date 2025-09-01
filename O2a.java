63fc2627b54df67b6913a14b97ad418f8b6916b7
hovringmarkus-crypto <hovringmarkus@gmail.com>
2025-09-01 22:46:47 +0200

    Add files via upload

---
+++ O2a.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class O2a {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Skriv inn poengsum som heltall: ");
+		
+		int poengsum = scanner.nextInt();
+		scanner.close();
+		
+		if (poengsum < 0 || poengsum > 100) {
+			System.out.println("ugyldig poengsum (negativ verdi eller over 100)");
+			return;
+		}
+		
+		char karakter;
+		
+		if (poengsum >=90) {
+			karakter = 'A';
+		} else if (poengsum >= 80) {
+			karakter = 'B';
+		} else if (poengsum >= 60) {
+			karakter = 'C';
+		} else if (poengsum >= 50) {
+			karakter = 'D';
+		} else if (poengsum >= 40) {
+			karakter = 'E';
+		} else {
+			karakter = 'F';
+		}
+		
+		System.out.println("Karakter: " + karakter);
+	}
+
+}
